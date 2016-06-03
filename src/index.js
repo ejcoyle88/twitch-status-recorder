8415088b456fce06ac212dd8448ee6d89055a674
Elliot Coyle <elliot@vaetek.biz>
2016-06-03 18:11:05 +0100

    Getting it working :D

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var util = require('util');
 var request = require('request');
 var pg = require('pg');
+var _ = require('lodash');
 
 pg.defaults.ssl = true;
 
@@ -43,19 +44,46 @@ function getNextStatuses(names, cb) {
 function getCurrentStatuses(names, cb) {
   withDb(function(db) {
     var nameCsv = names.map(function(n) { return '\'' + n + '\''; }).join();
-    console.log(nameCsv);
-    db.query('SELECT DISTINCT ON (s.id) ss.* FROM streamer_status ss INNER JOIN streamer s ON s.id = ss.streamer_id WHERE s.name IN (' + nameCsv + ') ORDER BY s.id, ss.timestamp')
+    db.query('SELECT DISTINCT ON (s.id) ss.*, s.name FROM streamer_status ss INNER JOIN streamer s ON s.id = ss.streamer_id WHERE s.name IN (' + nameCsv + ') ORDER BY s.id, ss.timestamp')
       .on('row', function(row, result) { result.addRow(row); })
       .on('end', function(result) { cb(result.rows); });
   });
 }
 
-function getDifferingStatuses(statuses, nextStatuses) {
+function getDifferingStatuses(statuses, nextStatusesJSON) {
+  var nextStatuses = JSON.parse(nextStatusesJSON);
+  var statusesLength = statuses.length;
+  var result = [];
+  for(var i = 0; i < statusesLength; i++) {
+    var prevStatus = statuses[i];
+    var nextStatus = _.filter(nextStatuses.streams, function(s) { return s.channel.display_name === prevStatus.name; });
+    if(prevStatus == null || !_.isEqual(prevStatus.status, nextStatus)) {
+      result.push(nextStatus);
+    }
+  }
   return nextStatuses;
 }
 
+function getStreamerId(name, cb) {
+  withDb(function(db) {
+    db.query('SELECT id FROM streamer WHERE name = \'' + name.toLowerCase() + '\'')
+      .on('row', function(row, result) { result.addRow(row); })
+      .on('end', function(result) { cb(result.rows[0].id); });
+  });
+}
+
 function saveStatuses(statuses) {
-  console.log(statuses);
+  var statusesLength = statuses.streams.length;
+  for(var i = 0; i < statusesLength; i++) {
+    var status = statuses.streams[i];
+    var name = status.channel.display_name;
+    var statusJSON = JSON.stringify(status);
+    getStreamerId(name, function(id) {
+      withDb(function(db) {
+        db.query('INSERT INTO streamer_status (status, streamer_id, timestamp) VALUES (\'' + statusJSON + '\', ' + id + ', CURRENT_TIMESTAMP);');
+      });
+    });
+  }
 }
 
 function getNames(cb) {
